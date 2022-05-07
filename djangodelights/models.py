d47efdae46f090b178b78e1e6f289fb2d8dd87d3
Allison D <aadwyre@gmail.com>
2022-05-06 23:07:56 -0500

    create models

--- djangodelights/models.py
+++ djangodelights/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.FloatField()
+    CUPS = 'CUPS'
+    POUNDS = 'LBS'
+    TABLESPOONS = 'TBSP'
+    TEASPOONS = 'TSP'
+    OUNCES = 'OZ'
+    GRAMS = 'GS'
+    EGGS = 'EGGS'
+    UNITS=[
+        (CUPS, 'cups'),
+        (POUNDS, 'lbs'),
+        (TABLESPOONS, 'tbsp'),
+        (TEASPOONS, 'tsp'),
+        (OUNCES, 'ounces'),
+        (GRAMS, 'grams'),
+        (EGGS, 'eggs')
+    ]
+    unit = models.CharField(max_length=4, choices=UNITS, default=CUPS)
+    unit_price = models.FloatField()
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
