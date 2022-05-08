bf487a7a4a60cb789cce554b4379e51b5223b8e9
Allison D <aadwyre@gmail.com>
2022-05-07 20:28:23 -0500

    create menu list view

--- djangodelights/urls.py
+++ djangodelights/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("menu/list", views.MenuListView.as_view(), name="menu_list"),
 ]
--- djangodelights/views.py
+++ djangodelights/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import MenuItem, Ingredient, RecipeRequirement, Purchase
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "djangodelights/index.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["ingredients"] = Ingredient.objects.all()
+        context["menuitems"] = MenuItem.objects.all()
+        context["reqs"] = RecipeRequirement.objects.all()
+        context["purchases"] = Purchase.objects.all()
+        return context
+
+class MenuListView(ListView):
+    model = MenuItem
+    template_name = "djangodelights/menu_list.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["menuitems"] = MenuItem.objects.all()
+        return context
